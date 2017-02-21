d943773a547fc9f1fe3871ee575dbf77560035a7
yy550956983 <550956983@qq.com>
2017-02-21 17:14:15 +0800

    snap test

--- snapTest/line.js
+++ snapTest/line.js
@@ -1,14 +1,29 @@
-var Line=(function(){
+var Line=(function(Model,svg){
 	var toJson=JSON.stringify;
 	
-	function Line(fromId,toId){
-		this.fromId=fromId;
-		this.toId=toId;
+	function Line(m1,m2){
+		this._fromId=m1._id;
+		this._toId=m2._id;
+		this._line=draw(m1,m2);
 	}
 	
 	var proto=Line.prototype;
 	
-	proto.update=function(model){
+	function draw(fromModel,toModel){
+		var ltm=leftTopModel(fromModel,toModel);
+		var rbm=rightBottomModel(fromModel,toModel);
+		
+		var p1=ltm._rightMid();
+		var p2=rbm._leftMid();
+		
+		return svg.paper.line(p1.x,p1.y,p2.x,p2.y);
+	}
+	
+	proto.redraw=function(){
+		_line.attr({x1:p1.x,y1:p1.y,x2:p2:x,y2:p2.y});
+	}
+			
+	function leftTopModel(m1,m2){
 		
 	}
 	
@@ -17,4 +32,4 @@ var Line=(function(){
 	}
 	
 	return Line;
-})();
+})(Model,svg);
