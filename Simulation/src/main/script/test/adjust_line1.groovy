7caf31bc82cddfb2186a48ab2ef2e1dbcc7b031c
yy550956983 <550956983@qq.com>
2017-03-30 02:15:57 +0800

    api update

--- Simulation/src/main/script/test/adjust_line1.groovy
+++ Simulation/src/main/script/test/adjust_line1.groovy
@@ -1,5 +1,85 @@
 import groovy.transform.TupleConstructor
 
+@TupleConstructor
+class Stage{
+	Map remaining
+	Map existing
+	Map tails
+
+	List<Stage> split(){
+		def result=[]
+		if(tails.size()<=1){
+			result<<this
+		}else{
+			tails.each{
+				result<<new Stage(remaining.clone(),existing.clone(),[:]<<it)
+			}
+		}
+		result
+	}
+
+	def resolveToBranch(){
+		if(tails.size()!=1){
+			return
+		}
+		while(true){
+			def count=tails.size()
+
+			if(count==0){
+				tails=[:]
+				break
+			}else if(count==1){
+				def added=tails.find{true} // only one
+				remaining.remove(added.key)
+				existing<<added
+			}else/* if(count>1) */{
+				break
+			}
+			tails=remaining.findAll{it.value[0]==tails.find{true}.value[1]}
+		}
+	}
+
+	Stage resolveMainChain(){
+		List<Stage> branches=([]<<this)
+
+		while(branches.any{it.tails}){
+			branches.each{ it.resolveToBranch() }
+			branches=(branches as Iterable<Range>).collectMany{it.split()}
+		}
+		return (branches as Iterable<Stage>).max{it.existing.size()}
+	}
+}
+
+static def resumeIt(List<Stage> stages,List resolved){
+	stages.each{
+		if(!it.tails){
+			resolved.add(it)
+		}else{
+			it.resolveToBranch()
+			if(it.tails.size()>1){
+				def found=it.split()
+				resumeIt(found,resolved)
+			}else if(it.tails.size()==0){
+				resolved.add(it)
+			}
+		}
+	}
+	return resolved
+}
+
+static List<Stage> resumeStages(List<Stage> stages){
+	def resolved=[]
+	resumeIt(stages,resolved)
+	resolved
+}
+
+static def resolveOnce(Stage initStage){
+	initStage.resolveToBranch()
+	def initChains=initStage.split()
+	def chains=resumeStages(initChains)
+	return (chains as Iterable<Stage>).max{it.existing.size()}
+}
+
 def origin=[
 	l1:['s1', 'j1'],
 	l10:['b6', 'j1'],
@@ -13,123 +93,17 @@ def origin=[
 	l8:['b3', 'b5'],
 	l9:['b5', 'b6']]
 
-@TupleConstructor
-class Stage{
-	Map remaining
-	Map existing
-	Map tails
+def initStage=new Stage(origin,[:],origin.findAll{it.key=='l1'})
+println initStage.tails
 
-	def split(){
-		def result=[]
-		if(tails.size()<=1){
-			result<<this
-		}else{
-			tails.each{
-				result<<new Stage(remaining.clone(),existing.clone(),[:]<<it)
-			}
-		}
-		result
-	}
+def mainChain=initStage.resolveMainChain()
+println mainChain.existing
+println mainChain.remaining
 
-	def resolveToBranch(){
-		assert tails.size()==1
-
-		existing<<tails
-		remaining.remove(tails.find{true}.key)
-		while(true){
-			tails=remaining.findAll{it.value[0]==tails.find{true}.value[1]}
-			def count=tails.size()
-
-			if(count==0){
-				tails=[:]
-				break
-			}else if(count==1){
-				def added=tails.find{true} // only one
-				remaining.remove(added.key)
-				existing<<added
-			}else/* if(count>1) */{
-				break
-			}
-		}
-	}
-	
-	
-}
-
-def resumeIt(stages,List resolved){
-	
-}
+println initStage.tails
 
 
 
-def resumeResolve(chains){
-	def resolved=[]
-	resume(chains,resolved)
-	return resolved
-}
-
-def resume(chains, List resolved){
-	chains.each{
-		if(!it.tails){
-			resolved.add(it)
-		}else{
-			def found=resolve(it.remaining, it.existing, it.tails)
-			if(found.tails.size()>1){
-				found=splitFound(found)
-				resume(found,resolved)
-			}else if(found.tails.size()==0){
-				resolved.add(found)
-			}
-		}
-	}
-	return resolved
-}
-
-def resolveAll(origin,start){
-	return resolveChain(origin){it.key==start}
-}
-
-def resolveRemaining(remaining){
-	def result=[]
-
-	def remain=remaining.clone()
-
-	while(remain){
-		def mainChain=resolveChain(remain){true}
-		result<<mainChain.existing
-		remain=mainChain.remaining
-	}
-	result
-}
-
-def resolveChain(origin,Closure start){
-	def initFound=resolve(origin,[:],origin.find(start))
-
-	def initChains=splitFound(initFound)
-	def chains=resumeResolve(initChains)
-	def mainChain=chains.max{it.existing.size()}
-
-	return mainChain
-}
-
-def resolveIt(origin,start){
-	def chains=[]
-
-	def remain=origin.clone()
-	def clue=start
-
-	while(remain){
-		def mainChain=resolveChain(remain,clue)
-		chains<<mainChain.existing
-		remain=mainChain.remaining
-		clue={true}
-	}
-	def res=[:]
-	chains.each{
-		it.each{res<<it}
-	}
-	return res
-}
 
 
 
