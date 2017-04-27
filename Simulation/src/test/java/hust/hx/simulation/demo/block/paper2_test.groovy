c7147a33804d3de902a300fca200b8f9809719f8
yy550956983 <550956983@qq.com>
2017-04-27 08:29:11 +0800

    submit update

--- Simulation/src/test/java/hust/hx/simulation/demo/block/paper2_test.groovy
+++ Simulation/src/test/java/hust/hx/simulation/demo/block/paper2_test.groovy
@@ -1,5 +1,6 @@
 package hust.hx.simulation.demo.block
 
+import hust.hx.simulation.util.PrintUtil
 import hust.hx.util.TestUtil
 
 def fitness(List origin,List output){
@@ -11,15 +12,19 @@ def fitness(List origin,List output){
 }
 
 def rs=new RegularSystem(0.8, 3.36, 0.2, 0.05)
-
 TestUtil.timeIt{rs.simulate()}
-
 def out0=rs.output
 
-def rs1=new RegularSystem(0.8, 5, 0.2, 0.05)
+def rs1=new RegularSystem(0.6009873553, 4.4080055403, 0.2699913743, 0.3074793669)
 rs1.simulate()
 def out1=rs1.output
 
-println out1.size()
-println out0.size()
-println fitness(out0,out1)
+println 'fitness: '+fitness(out0,out1)
+
+def time=rs1.time
+def output=rs1.output
+PrintUtil.print{p->
+	time.eachWithIndex{v,i->
+		p.println("$v ${output[i]}")
+	}
+}
